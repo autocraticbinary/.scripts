e4d80fd5ace751dcfe74b45f3ec273212a7c639b
b1n4ryw0rm <b1n4ryw0rm@gmail.com>
2023-10-15 19:42:02 +0530

    dwmbar-colored added

--- loop-alert-bl.py
+++
@@ -1,9 +0,0 @@
-import time
-import subprocess
-
-while True:
-    # Run your command here
-    subprocess.run(["alert-batterylow"])
-
-    # Sleep for a specified interval (e.g., 1 hour)
-    time.sleep(60)  # 3600 seconds = 1 hour
