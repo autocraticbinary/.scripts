e29a8ed4be3d17ed179d249a364b3f0061cb3111
Autocratic Binary <autocraticbinary@gmail.com>
2024-08-27 19:56:58 +0530

    change scripts

--- status.sh
+++ status.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+cpu () {
+  top -bn1 | awk '/Cpu/ { print $2}'
+}
+
+temp () {
+  sensors | awk '/Core 0/ {print "TEMP "  $3}'
+}
+
+disk () {
+  df -h /home | awk ' /[0-9]/ {print "DISK " $3 "/" $2}'
+}
+
+mem () {
+  free --mebi | sed -n '2{p;q}' | awk '{printf ("MEM %2.2fGiB/%2.2fGiB\n", ( $3 / 1024), ($2 / 1024))}'
+}
+
 battery () {
 	~/.scripts/battery
 }
@@ -25,6 +41,6 @@ vol () {
 }
 
 while :; do
-	xsetroot -name "[$(battery)]  <<  $(wifi)  <<  LIT $(lit)  <<  VOL $(vol)  <<  $(clock) "
+	xsetroot -name "[$(battery)]  <<  $(wifi)  <<  LIT $(lit)  <<  VOL $(vol) << $(temp) << $(disk)  << $(mem) << $(clock) "
 	sleep 1s
 done
